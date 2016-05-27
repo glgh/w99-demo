3938a87c5fcdf5f3a689a319ea735dee44f118ab
GL <gl512@georgetown.edu>
2016-05-26 23:47:07 -0400

    Modified fail message

--- js/sys-status.js
+++ js/sys-status.js
@@ -1,6 +1,6 @@
 function updateSystemStatus(status) {
   if (status.failed) {
-    var msg = '<font color="red">Failed!</font>'
+    var msg = '<font color="red">Simulation FAILED due to collision!</font> Press "Reset" to continue.'
   } else {
     var msg = 'Normal'
   }
